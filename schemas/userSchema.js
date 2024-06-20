bebad012736b0bdfe85ad94cfef3d8ea6cad9758
boben195 <boben195@ukr.net>
2024-06-20 19:30:09 +0300

    add helpers, schemas and other

---
+++ schemas/userSchema.js
@@ -0,0 +1,16 @@
+import Joi from "joi";
+
+export const registerSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+  passwordConform: Joi.string().required(),
+});
+
+export const loginSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+});
+
+export const verifySchema = Joi.object({
+  email: Joi.string().required().messages({ 'any.required': 'missing required field email' }),
+});
