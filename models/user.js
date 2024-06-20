11be2e39c8196fea17100d69d64d9a6d4ed6d545
boben195 <boben195@ukr.net>
2024-06-20 18:42:17 +0300

    add main files and folders

---
+++ models/user.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const contactSchema = new mongoose.Schema(
+  {
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+        passwordConform: {
+            type: String,
+            required: [true, 'Password must be confirmed']
+        },
+        weight: {
+            type: String,
+        },
+        gender: {
+            type: String,
+            enum: ['male', 'female']
+        },
+        activeTimeSport: {
+            type: String,
+        },
+        dailyWaterRate: {
+            type: String,
+        },
+  token: {
+    type: String,
+    default: null,
+    },
+    avatarURL: String,
+  verify: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+    required: [true, 'Verify token is required'],
+  },
+}, {
+    versionKey: false
+  }
+);
+
+export default mongoose.model("User", contactSchema);
