f2044282463b4802c38d66ab96f71f5a9038baa9
boben195 <boben195@ukr.net>
2024-06-22 11:43:03 +0300

    add login, logout, getAll

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,13 +3,18 @@ import express from "express";
 import validateBody from "../helpers/validateBody.js";
 import userServices from "../controllers/authControler.js";
 import { registerSchema, loginSchema } from "../schemas/userSchema.js";
+import {auth} from "../middlewares/auth.js"
 
 const usersRouter = express.Router();
 
 usersRouter.post("/register", validateBody(registerSchema), userServices.registerUser);
 
-usersRouter.post("/login", validateBody(loginSchema), userServices.loginUser);
+usersRouter.post("/login", validateBody(loginSchema), userServices.login);
 
-usersRouter.post("/logout", userServices.logoutUser);
+usersRouter.post("/logout/", auth, userServices.logout);
+
+usersRouter.get("/current/", auth, userServices.currentUser);
+
+usersRouter.get("/all", userServices.getAllUsers);
     
 export default usersRouter;
