6e08218094434e5ff5e761ae0412d6e9a70c8f6e
Viktor Kudryavtsev <viktork8888@gmail.com>
2024-06-25 22:26:12 +0300

    refactoring swagger

--- swagger.js
+++ swagger.js
@@ -6,6 +6,7 @@ const API_URL = process.env.API_URL;
 const swaggerFile = path.resolve('swagger.json');
 const swaggerDocument = JSON.parse(fs.readFileSync(swaggerFile, 'utf8'));
 
+// Replaces default local url to production server url
 if (process.env.RUN_MODE === 'PROD') {
     swaggerDocument.servers[0].url = API_URL;
 }
